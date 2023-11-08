bf75117a126dfd6bf1fb3dbe28ebb623c46e6a74
Ujjawal Singh Tolia <126862196+Ujju-27@users.noreply.github.com>
2023-11-08 09:04:45 +0530

    add files via upload

---
+++ Lagranges.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main(){
+    int n ;
+    printf("Enter the number of terms= ");
+    scanf("%d", &n);
+
+    float X[n] , Y[n] , sum = 0 , x;
+
+    printf("Enter the values of X = ");
+    for(int i = 0 ; i < n ; i++)
+        scanf("%f" , &X[i]);
+
+    printf("Enter the values of Y = ");
+    for(int i = 0 ; i < n ; i++)
+        scanf("%f" , &Y[i]);
+
+    printf("Enter the values of x which you want find y = ");
+    scanf("%f" , &x);
+
+    //Main logic of lagrnges
+    for(int i = 0 ; i < n ; i++)
+    {
+        float product = 1;
+        for(int j = 0 ; j < n ; j++)
+        {
+            if(i != j)
+            {
+                product = product * ((x - X[j])/(X[i] - X[j])); 
+            }
+        } 
+        sum = sum + (product * Y[i]);
+    }
+    printf("\nValue of x is %0.2f and Sum is = %0.2f\n" , x , sum);
+    return 0;
+}
